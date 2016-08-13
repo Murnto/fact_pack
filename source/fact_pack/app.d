e127521d3413e243987ed8ea7e72b890b6c93d26
David <Murnto@users.noreply.github.com>
2016-08-13 22:41:10 +1000

    Cleanup app main

--- source/fact_pack/app.d
+++ source/fact_pack/app.d
@@ -11,38 +11,33 @@ void test(ref Packdata pd)
     // writeln("copper-ore=", pd.resources["copper-ore"].title);
 }
 
+void testDynamicPacks()
+{
+    foreach (ref DirEntry f; dirEntries("pack", SpanMode.shallow))
+    {
+        writeln(f);
+        if (f.isFile)
+        {
+            string redir = chomp(readText(f));
+            writeln("redir = ", redir);
+        }
+        if (f.isDir)
+        {
+            StopWatch sw;
+            sw.start();
+            auto pd = new Packdata(f.name ~ "/");
+            sw.stop();
+            test(pd);
+
+            writeln("  took ", sw.peek().msecs, " ms");
+        }
+    }
+}
+
 void main()
 {
+    // testDynamicPacks();
 
-    // foreach (ref DirEntry f; dirEntries("pack", SpanMode.shallow))
-    // {
-    //     writeln(f);
-    //     if (f.isFile)
-    //     {
-    //         string redir = chomp(readText(f));
-    //         writeln("redir = ", redir);
-    //     }
-    //     if (f.isDir)
-    //     {
-	// 		StopWatch sw;
-	// 		sw.start();
-    //         auto pd = new Packdata(f.name ~ "/");
-	// 		sw.stop();
-	// 		test(pd);
-
-	// 		writeln("  took ", sw.peek().msecs, " ms");
-    //     }
-    // }
-
-    auto pd = new Packdata("/home/dave/dev/factorio/pack/base-f12/");
+    auto pd = new Packdata("pack/base-f12/");
     test(pd);
-    // pd = new Packdata("/home/dave/dev/factorio/pack/base-f13/");
-    // test(pd);
-    // pd = new Packdata("/home/dave/dev/factorio/pack/dytech-f12/");
-    // pd = new Packdata("/home/dave/dev/factorio/pack/5dim-f12/");
-    // pd = new Packdata("/home/dave/dev/factorio/pack/bobmods-f12/");
-    // pd = new Packdata("/home/dave/dev/factorio/pack/mopack-f12/");
-    // pd = new Packdata("/home/dave/dev/factorio/pack/yuoki-f12/");
-    // pd = new Packdata("/home/dave/dev/factorio/pack/yuoki-f12-a/");
-    // writeln(pd.find_recipes_using("item", "iron-plate"));
 }
